da9f2d03318887634e4b00130339bd143c106b25
JENNY-257 <j.iribagiza2@gmail.com>
2023-09-02 00:02:15 +0200

    user generate token

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,59 +1,59 @@
-import User from '../models/userModel.js'; 
-import bcrypt from 'bcrypt'; 
+import User from '../models/userModel.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
 
 const signup = async (req, res) => {
-    try {
-      const { name, email, password } = req.body;
-      const existingUser = await User.findOne({ email });
-      if (existingUser) {
-        return res.status(400).json({ message: 'User already exists' });
-      }
-  
-      const saltRounds = 10;
-      const hashedPassword = await bcrypt.hash(password, saltRounds);
-  
-      const newUser = new User({
-        name,
-        email,
-        password: hashedPassword,
-      });
-  
-      await newUser.save();
-  
-      return res.status(201).json({ message: 'Signup successful' });
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ message: 'Internal server error' });
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' });
     }
-  };
-  
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET);
+
+    return res.status(201).json({ message: 'Signup successful', token });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
 
 const login = async (req, res) => {
-    try {
+  try {
+    const { email, password } = req.body;
 
-      const { email, password } = req.body;
-  
-      
-      const user = await User.findOne({ email });
+    const user = await User.findOne({ email });
 
-      if (!user) {
-        return res.status(401).json({ message: 'Invalid email or password' });
-      }
-  
-      const passwordMatch = await bcrypt.compare(password, user.password);
-  
-      if (passwordMatch) {
-        return res.status(200).json({ message: 'Login successful' });
-      } else {
-        return res.status(401).json({ message: 'Invalid email or password' });
-      }
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ message: 'Internal server error' });
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid email or password' });
     }
-  };
 
+    const passwordMatch = await bcrypt.compare(password, user.password);
 
-  export {signup, login} ;
+    if (passwordMatch) {
+      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
 
+      return res.status(200).json({ message: 'Login successful', token });
+    } else {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
 
+export { signup, login };
